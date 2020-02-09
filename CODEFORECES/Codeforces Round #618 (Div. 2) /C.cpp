1a036c955a85f4bdaf953dc75b510f70b3a2ab57
박성빈 <sungbinpark@bagseongbin-ui-MacBookPro.local>
2020-02-10 07:06:09 +0900

    C. Anu Has a Function(bit)

---
+++ CODEFORECES/Codeforces Round #618 (Div. 2) /C.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+
+using namespace std;
+
+int a[100001],d[32];
+
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int n;
+    cin >> n;
+    for(int i=0;i<n;i++){
+        cin >> a[i];
+        for(int j=31;j>=0;j--){
+            if((1<<j)&a[i])d[j]++;
+        }
+    }
+    int ret = -1;
+    for(int j=31;j>=0;j--){
+        if(d[j]==1){
+            for(int i=0;i<n;i++){
+                if((1<<j)&a[i]){
+                    cout << a[i] << ' ';
+                    ret = i;
+                    break;
+                }
+            }
+        }
+        if(ret!=-1)break;
+    }
+    for(int i=0;i<n;i++){
+        if(ret==i)continue;
+        cout << a[i] << ' ';
+    }
+}

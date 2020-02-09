e45ff2d0418942be8d17730179e168799d949b57
박성빈 <sungbinpark@bagseongbin-ui-MacBookPro.local>
2020-02-10 07:14:22 +0900

    A. Non-zero

---
+++ CODEFORECES/Codeforces Round #618 (Div. 2) /A.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+ 
+using namespace std;
+ 
+int a[101];
+ 
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+ 
+    int t;
+    cin >> t;
+    while(t--){
+        int n;
+        int ret = 0;
+        cin >> n;
+        int sum=0;
+        for(int i=0;i<n;i++){
+            cin >> a[i];
+            if(!a[i]){
+                sum++;
+                ret++;
+            }
+            else sum+=a[i];
+        }
+        if(!sum){
+            ret++;
+        }
+        cout << ret << '\n';
+    }
+}

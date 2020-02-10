36e1e8ccaa5698f9f01e4b1f5bca21f1bfdf5fc0
박성빈 <sungbinpark@bagseongbin-ui-MacBookPro.local>
2020-02-10 20:38:52 +0900

    연산자 끼워넣기(삼성기출)  noj.am

---
+++ BOJ/BACKTRACKING/14888.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+int a[11],op[4];
+
+pair<int,int> foo(int idx){
+    pair<int,int> ret = {-1e9,1e9};
+    if(!idx)return {a[idx],a[idx]};
+    
+    for(int i=0;i<4;i++){
+        if(!op[i])continue;
+        op[i]--;
+        if(i==0)ret = {max(ret.first,foo(idx-1).first + a[idx]),min(ret.second,foo(idx-1).second + a[idx])}; 
+        if(i==1)ret = {max(ret.first,foo(idx-1).first - a[idx]),min(ret.second,foo(idx-1).second - a[idx])}; 
+        if(i==2)ret = {max(ret.first,foo(idx-1).first * a[idx]),min(ret.second,foo(idx-1).second * a[idx])};
+        if(i==3 && a[idx])ret = {max(ret.first,foo(idx-1).first / a[idx]),min(ret.second,foo(idx-1).second / a[idx])};
+        op[i]++;
+    }
+    return ret;
+}
+
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int n;
+    cin >> n;
+    for(int i=0;i<n;i++)cin >> a[i];
+    for(int i=0;i<4;i++)cin >> op[i];
+    auto ret = foo(n-1);
+    cout << ret.first << '\n' << ret.second << '\n';
+}
